e95deb98e63a4c858d020bacf77910dc06d8af5b
bharath <bharath2103@gmail.com>
2024-08-23 21:37:38 +0100

    refactor

--- Voting/vote.sol
+++ Voting/vote.sol
@@ -6,17 +6,24 @@ contract voting {
 
     struct Party {
         string name;
-        address self_address;
+        uint receivedVote;
+    }
+
+    struct Voter {
+        address selfAddress;
         uint vote;
     }
 
     //contractowner: 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4
-    //Party A: 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2
-    //Party B: 0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db
+    //voter 1: 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2
+    //voter 2: 0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db
+    //voter 3: 0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB
 
     Party[] parties;
+    Voter[] voters;
     address contractOwner;
-    mapping(string => int) public partyindex;
+    mapping(string => int) public partyLookup;
+    bool pollingStart = false;
     int n;
 
     constructor() {
@@ -24,10 +31,23 @@ contract voting {
     }
 
     //Only contract owner can register the party to contest
-    function register(string memory _name, address _wallet) public onlyContractOwner {
-        parties.push(Party({name: _name, self_address: address(_wallet), vote: 0}));
-        partyindex[_name] = n;
+    function partyRegistration(string memory _name) public onlyContractOwner {
+        require(pollingStart == false, "Polling has started, cannot register new party");
+        parties.push(Party({name: _name, receivedVote: 0}));
+        partyLookup[_name] = n;
         n++;
+    }
+
+    function PollingStart() public onlyContractOwner {
+        pollingStart = true;
+    }
+
+    function PollingStop() public onlyContractOwner {
+        pollingStart = false;
+    }
+
+    function voterRegistration(address voter) public onlyContractOwner {
+        voters.push(Voter({selfAddress: voter, vote: 1}));
     }
 
     modifier onlyContractOwner() {
@@ -35,5 +55,17 @@ contract voting {
         _;
     }
 
+    function notRegisteredOrCastedVoteAlready(address _voter) view private returns (bool) {
+        for(uint i = 0; i <= voters.length; i++) {
+            if( voters[i].selfAddress == _voter && voters[i].vote != 0) {
+                return true;
+            }
+        } 
+        return false;   
+    }
+
+    function vote() view public {
+        require(notRegisteredOrCastedVoteAlready(msg.sender) == true, "Voter is either not registered or has casted vote already");
+    }
 }
 
